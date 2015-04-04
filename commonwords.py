4909fadc0e5323febf392435d9cee0e91515a29a
Tim Griffith <grifmang@gmail.com>
2015-04-03 21:28:19 -0400

    Fixed spacing issues.

--- commonwords.py
+++ commonwords.py
@@ -1,29 +1,29 @@
 def checkio(first, second):
     result = []
     
-	if len(first) > len(second):
+    if len(first) > len(second):
         
-		for word in first.split(','):
+        for word in first.split(','):
             
-			if word in second.split(','):
+            if word in second.split(','):
                 result.append(word)
         
-		return ','.join(sorted(result))
+        return ','.join(sorted(result))
     
-	elif len(second) > len(first):
+    elif len(second) > len(first):
         
-		for word in second.split(','):
+        for word in second.split(','):
             
-			if word in first.split(','):
+            if word in first.split(','):
                 result.append(word)
         
-		return ','.join(sorted(result))
+        return ','.join(sorted(result))
     
-	else:
+    else:
         
-		for word in first.split(','):
+        for word in first.split(','):
             
-			if word in second.split(','):
+            if word in second.split(','):
                 result.append(word)
         
-		return ','.join(sorted(result))
+        return ','.join(sorted(result))
